732ff46d5fe075156cbcbd50a802dd2d326716cd
Eugine <eugineogutu004@gmail.com>
2025-07-06 17:38:24 +0300

    modified:   Conditional calculations using IF.sql

--- Conditional calculations using IF.sql
+++ Conditional calculations using IF.sql
@@ -48,3 +48,22 @@ SELECT
 FROM access_to_basic_services;
 
 -- 3. Replace NULL values for remaining regions
+SELECT Region,
+       Pct_unemployment,
+       IF( (Region = "Central and Southern Asia") AND (Pct_unemployment IS NULL), 19.59,
+            IF( (Region = "Eastern and South-Eastern Asia") AND (Pct_unemployment IS NULL), 22.64,
+                 IF( (Region = "Europe and Northern America") AND (Pct_unemployment IS NULL), 24.43,
+                      IF( (Region = "Latin America and the Caribbean") AND (Pct_unemployment IS NULL), 24.23,
+                           IF( (Region = "Northern Africa and Western Asia") AND (Pct_unemployment IS NULL), 17.84,
+                                IF( (Region = "Oceania") AND (Pct_unemployment IS NULL), 4.98,
+                                     IF( (Region = "Sub-Saharan Africa") AND (Pct_unemployment IS NULL), 33.65,
+                                          -999
+                                         )
+                                    )
+                               )
+                          )
+                     )
+                )
+           )AS New_pct_unemployment
+FROM united_nations.Access_to_Basic_Services;
+
